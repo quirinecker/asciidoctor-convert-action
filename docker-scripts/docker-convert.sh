3a26b37e45bc07f3d2104b7aa36568759259e71e
QuirinEcker <quirin.ecker@bajupa.com>
2021-10-12 16:08:10 +0200

    just checking the inputs right now

--- docker-scripts/docker-convert.sh
+++ docker-scripts/docker-convert.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/bash
 
-source ./docker-convert-util.sh
+#source ./docker-convert-util.sh
 
 echo something is working
 echo "input => $INPUT_INPUTPATH"
 
-outputPath="output"
-inputPath="input"
-
-echo building html
-
-cp -r $inputPath/* $outputPath
-
-if [ $CONVERT_SLIDES = true ]; then
-    convertFolderToSlides "$inputPath/slides" "$outputPath/slides"
-fi
-
-outputPath="output"
-inputPath="input"
-
-convertFolderToHTML "$outputPath"
+#outputPath="output"
+#inputPath="input"
+#
+#echo building html
+#
+#cp -r $inputPath/* $outputPath
+#
+#if [ $CONVERT_SLIDES = true ]; then
+#    convertFolderToSlides "$inputPath/slides" "$outputPath/slides"
+#fi
+#
+#outputPath="output"
+#inputPath="input"
+#
+#convertFolderToHTML "$outputPath"
 
