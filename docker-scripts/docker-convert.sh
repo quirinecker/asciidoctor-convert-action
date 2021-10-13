a63d29302b112daac42262abb7136202ffb71e09
QuirinEcker <quirin.ecker@bajupa.com>
2021-10-13 08:30:36 +0200

    added parameter for action to specify the slides directory

--- docker-scripts/docker-convert.sh
+++ docker-scripts/docker-convert.sh
@@ -3,16 +3,20 @@
 source ./docker-scripts/docker-convert-util.sh
 inputPath="$INPUT_INPUTPATH"
 outputPath="$INPUT_OUTPUTPATH"
+slideInputPath="$INPUT_SLIDEINPUTPATH"
+slideOutputPath="$INPUT_SLIDEOUTPUTPATH"
 
 echo "input => $inputPath"
 echo "output => $outputPath"
+echo "slideInputPath => $slideInputPath"
+echo "slideOutputPath => $slideOutputPath"
 echo building html
 
 mkdir $outputPath
 cp -r $inputPath/* $outputPath
 
 if [ $INPUT_SLIDES = true ]; then
-    convertFolderToSlides "$inputPath/slides" "$outputPath/slides"
+    convertFolderToSlides "$slideInputPath" "$slideOutputPath"
 fi
 
 inputPath="$INPUT_INPUTPATH"
